4b7e409bf2f98046ee825fea893ce11a5c1edf26
Gustavo Azevedo <ghca12@gmail.com>
2025-09-01 16:04:23 -0300

    feat(all): criando os arquivos base para a conexão

--- back/db.php
+++ back/db.php
@@ -1,13 +1,1 @@
-<?php
-$host = "localhost";
-$dbname = "empresa";
-$user = "root";
-$pass = "";
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    die(json_encode(["error" => "Erro na conexão: " . $e->getMessage()]));
-}
-?>
